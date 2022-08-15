483e89680cf7ebf0a4648431af79bed01ab8a17a
Adriano Vieira da Silva Santana <74624979+AdrianoSilvaSantana@users.noreply.github.com>
2022-08-15 17:04:08 -0300

    Update GridItem.tsx

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -15,11 +15,21 @@ export const GridItem = ({ item }: Props) => {
             <div className={styles.gridIcon}>
                 <img src={item.icon === 'up' ? upImage : downImage} alt="" width="30" />
             </div>
-            <div className={styles.gridTitle}></div>
-            <div className={styles.gridInfo}></div>
+            <div className={styles.gridTitle}>{item.title}</div>
+
+            {item.yourImc &&
+            <div className={styles.yourImc}>Seu IMC é de {item.yourImc} Kg/m²</div>
+            }
+
+            <div className={styles.gridInfo}>
+                <>
+                 IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong> 
+                </>
+            </div>
         </div>
     );
 }
+
 
 
 

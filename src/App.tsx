cf0b4a1fd2aa42fa782cb8e7c26eceb510a91944
Adriano Vieira da Silva Santana <74624979+AdrianoSilvaSantana@users.noreply.github.com>
2022-08-16 16:00:05 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 import poweredImage from './assets/powered.png';
+import leftArrowImage from './assets/leftarrow.png';
 import { GridItem } from './components/GridItem';
 
 import { levels, calculateImc, Level } from './helpers/imc';
@@ -17,6 +18,13 @@ const App = () => {
       alert("Digite todos os campos.")
     }
   }
+
+  const handleBackButton = () => {
+     setToShow(null);
+     setHeightField(0);
+     setWeightField(0);   
+  }
+
   return (
     <div className={styles.main}>
       <header>
@@ -33,14 +41,16 @@ const App = () => {
           placeholder='Digite a sua altura. Ex: 1.5 (em metros)' 
           value={heightField > 0 ? heightField : ''}
           onChange={e => setHeightField(parseFloat(e.target.value))}
+          disabled={toShow ? true : false}
           />
           <input type="number"
           placeholder='Digite o seu peso. Ex: 75.3 (em kg)' 
           value={WeightField > 0 ? WeightField : ''}
           onChange={e => setWeightField(parseFloat(e.target.value))}
+          disabled={toShow ? true : false}
           />
 
-          <button onClick={handleCalculateButton}>Calcular</button>
+          <button onClick={handleCalculateButton} disabled={toShow ? true : false}>Calcular</button>
         </div>
         <div className={styles.rightSide}>
           {!toShow && 
@@ -52,7 +62,9 @@ const App = () => {
           }
           {toShow && 
             <div className={styles.rightBig}>
-              <div className={styles.rightArrow}></div>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrowImage} alt="" width={25}/>
+              </div>
               <GridItem item={toShow} />
 
             </div> 
